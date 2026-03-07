d548627433c3c583fb4a7b34c1e4cb4a5ae88fdd
1102581553 <pheyeji@gmail.com>
2026-03-07 19:21:53 +0800

    Update DimensionParallel.h

--- src/DimensionParallel.h
+++ src/DimensionParallel.h
@@ -11,7 +11,7 @@
 #include <unordered_map>
 #include <queue>
 #include <string>
-#include <mc/world/actor/ActorUniqueID.h>
+#include <mc/legacy/ActorUniqueID.h>
 #include <mc/world/level/dimension/Dimension.h>
 #include <mc/world/actor/player/Player.h>
 
